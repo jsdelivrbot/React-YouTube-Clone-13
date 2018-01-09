2dd386727df96f3d447ac79bf4f81700d72c29c0
Andrew Grzegorek <andrewgrz@gmail.com>
2018-01-09 12:55:42 -0600

    Added search bar component and rendered it on index dom

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyARsRzcMi3gHRqYGq0HnoVVz0xFPWb6y2M';
 
 // Create a new component. This component should produce some HTML.
 // const App = () => {} is the same as const App = function() {}
 
 const App = () => {
-  return <div>Hi there</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 // Take this component's generated HTML and insert it into the DOM
 ReactDOM.render(<App />, document.querySelector('.container'))
