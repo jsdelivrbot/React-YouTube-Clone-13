e9f57ced3d27e0179ba4c17e90467a9a9f79478b
Andrew Grzegorek <andrewgrz@gmail.com>
2018-01-09 16:00:08 -0600

    Turned app into Class component and included the YouTube Search component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyARsRzcMi3gHRqYGq0HnoVVz0xFPWb6y2M';
 
-// Create a new component. This component should produce some HTML.
-// const App = () => {} is the same as const App = function() {}
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state= { videos:[] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }); //only works when key and property are identical
+      // this.setState({ videos: videos })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
-// Take this component's generated HTML and insert it into the DOM
+
 ReactDOM.render(<App />, document.querySelector('.container'))
