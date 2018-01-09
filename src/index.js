747e91125aab9e2d6f8cf6bb8dd06509aa54d31e
Andrew Grzegorek <andrewgrz@gmail.com>
2018-01-09 12:45:26 -0600

    Added components folder and files. Included YouTube API key and installed youtube-api-search package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyARsRzcMi3gHRqYGq0HnoVVz0xFPWb6y2M';
+
 // Create a new component. This component should produce some HTML.
-const App = function() {
+// const App = () => {} is the same as const App = function() {}
+
+const App = () => {
   return <div>Hi there</div>;
 }
 // Take this component's generated HTML and insert it into the DOM
-ReactDom.render(App)
+ReactDOM.render(<App />, document.querySelector('.container'))
